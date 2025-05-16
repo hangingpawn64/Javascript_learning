85e0aee6b7e42ff607bb817c6dbf06fdb33f5ad1
AkshitTupkar <2024bcs156@sggs.ac.in>
2025-05-17 02:44:38 +0530

    negative time issue fixed

--- Rmageddon Timer/script.js
+++ Rmageddon Timer/script.js
@@ -1,9 +1,7 @@
 function main() {
-  const dday = new Date("2025-09-10T18:30:00Z"); // i have adjusted the new date to so it matches ist in webpage
-  const ddayseconds = dday.getTime() / 1000; // will give dday in seconds
-
+  const dday = new Date("2025-11-09T18:30:00Z"); // i have adjusted the new date to so it matches ist in webpage
+  const ddayseconds = Math.floor(dday.getTime() / 1000); // will give dday in seconds
   let currentTime = Math.floor(Date.now() / 1000); // current time in seconds
-
   let secondLeft = ddayseconds - currentTime; // this is seconds left for dday
 
   // console.log("this is dday in seconds " + ddayseconds)  // line 8-10 are for checking
@@ -50,11 +48,19 @@ function main() {
         60
     ),
   };
-
+if (currentTime < ddayseconds){
   // we have got the data of time, now we will display that in frontend
   document.getElementById("nofdays").innerHTML = timer.days;
   document.getElementById("nofhours").innerHTML = timer.hours;
   document.getElementById("nofminutes").innerHTML = timer.minutes;
   document.getElementById("nofseconds").innerHTML = timer.seconds;
 }
+else{
+    document.getElementById("nofdays").innerHTML = "00";
+  document.getElementById("nofhours").innerHTML = "00";
+  document.getElementById("nofminutes").innerHTML = "00";
+  document.getElementById("nofseconds").innerHTML = "00";
+}
+}
+
 setInterval(main, 500);
