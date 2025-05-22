087f783c56b22c305e278146bff1f34d37946d23
AkshitTupkar <2024bcs156@sggs.ac.in>
2025-05-22 23:33:01 +0530

    task completed

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,1 +1,34 @@
+let screen = document.getElementById("screen");
+function appendNumber(number) {
+  if (screen.textContent == "0") {
+    screen.textContent = number;
+  } else {
+    screen.textContent += number;
+  }
+}
+
+function clearScreen() {
+  screen.textContent = "0";
+}
+
+function appendOperator(op) {
+  const lastChar = screen.textContent.slice(-1);
+  if ("+-*/".includes(lastChar)) {
+    screen.textContent = screen.textContent.slice(0, -1) + op;
+  } else {
+    screen.textContent += op;
+  }
+}
+
+function calculate(screen){
+    if ("+-*/".includes(screen.textContent.slice(-1))){
+        let newscreen = screen.textContent.slice(0,-1);
+    const result = eval(newscreen);
+    screen.textContent = result;
+}
+else{
+    const result = eval(screen.textContent);
+    screen.textContent = result;
+}
+}
 
